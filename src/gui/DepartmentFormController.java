8cf94b09fbc4340a67ac31d948263f41e0569166
Helielton Firoramonte <helifiora@gmail.com>
2019-02-15 12:45:52 -0200

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -25,7 +28,7 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 
 	private DepartmentService service;
-	
+
 	private List<DataChangeListener> dataChangeListeners = new ArrayList();
 
 	@FXML
@@ -50,16 +53,18 @@ public class DepartmentFormController implements Initializable {
 			throw new IllegalStateException("Entity was null");
 		if (service == null)
 			throw new IllegalStateException("Service was null");
-		
+
 		try {
 
 			entity = getFormData();
 			service.saveOrUpdate(entity);
 			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
-			
+
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		} catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
 		}
 	}
 
@@ -96,17 +101,34 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 
 		Department obj = new Department();
+		ValidationException exception = new ValidationException("Validation exception");
+
 		obj.setId(Utils.tryParseToInt(txtID.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals(""))
+			exception.addError("name", "Field can't be empty");
+
 		obj.setName(txtName.getText());
+
+		if (exception.getErrors().size() > 0)
+			throw exception;
+
 		return obj;
 	}
-	
+
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
-	
+
 	private void notifyDataChangeListeners() {
-		for(DataChangeListener listener : dataChangeListeners) {
+		for (DataChangeListener listener : dataChangeListeners) {
 			listener.onDataChanged();
 		}
 	}
